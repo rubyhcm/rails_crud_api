99b5fbde69f0beddf119f86177d6e4610799d320
nguyenloi <loindp@ipinc.io>
2023-10-25 11:51:07 +0700

    Remove - dockerfile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
   # fix ==> Response body is not available to scripts (Reason: CORS Missing Allow Origin)
   config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '*' # Thay đổi domain của trang web ReactJS của bạn
+      origins 'localhost:3000' # Thay đổi domain của trang web ReactJS của bạn
       resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
